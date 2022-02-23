983f86df23cdb8c9ef36fbc7b526f0e30c0eb5ee
Ben Blop <benblop@gmail.com>
2022-02-23 15:43:48 +0100

    add RPM measurment function

--- get_rpm.py
+++ get_rpm.py
@@ -1,5 +1,5 @@
-import encoder_driver_v2 as encodrv
-import arduino_driver_py3 as ardudrv
+import encoders_driver_v2 as encdrv
+import arduino_driver_v2 as ardudrv
 import sys
 import time
 
@@ -15,31 +15,53 @@ def delta_odo (odo1,odo0):
 if __name__ == "__main__":
     
     timeout = 1.0
-    cmdl = 40
-    cmdr = 40
-    duration = 20.0
-    tloop = 1.0 # 1 Hz loop
-    encodrv.set_baudrate(baudrate=115200)
-    serial_arduino, data_arduino = ardudrv.init_arduino_line()
-    ardudrv.send_arduino_cmd_motor(serial_arduino,cmdl,cmdr)
+    cmdl = 60
+    cmdr = 50
+    duration = 60.0
+    tloop = 5.0 # 1/5 Hz loop)
+    ard = ardudrv.ArduinoIO()
+    ard.send_arduino_cmd_motor (cmdl,cmdr)
+    encoddrv = encdrv.EncoderIO()      
 
-    sync0, timeAcq0, sensLeft0, sensRight0, posLeft0, posRight0 =  encodrv.read_single_packet(debug=True)
-    
+    #sync0, timeAcq0, sensLeft0, sensRight0, posLeft0, posRight0 =  encodrv.read_single_packet(debug=True)
+    while True:
+        sync0,data_encoders0 = encoddrv.read_packet(debug=True)
+        print (sync0)
+        if sync0:
+            break
+        time.sleep(0.1)
+
+    timeAcq0 = data_encoders0[0]
+    sensLeft0 = data_encoders0[1]
+    sensRight0 = data_encoders0[2]
+    posLeft0 = data_encoders0[3]
+    posRight0 = data_encoders0[4]
+    print (sync0, timeAcq0, sensLeft0, sensRight0, posLeft0, posRight0)
+
     t0 = time.time()
     while (time.time()-t0) < duration:
         time.sleep (tloop)
 
-        sync1, timeAcq1, sensLeft1, sensRight1, posLeft1, posRight1 =  encodrv.read_single_packet(debug=True)
+        #sync1, timeAcq1, sensLeft1, sensRight1, posLeft1, posRight1 =  encodrv.read_single_packet(debug=True)
+        sync1,data_encoders1 = encoddrv.read_packet(debug=True)
+        timeAcq1 = data_encoders1[0]
+        sensLeft1 = data_encoders1[1]
+        sensRight1 = data_encoders1[2]
+        posLeft1 = data_encoders1[3]
+        posRight1 = data_encoders1[4]
+
         #print sync1, timeAcq1, sensLeft1, sensRight1, posLeft1, posRight1
         print ("dTime",timeAcq0,timeAcq1,timeAcq1-timeAcq0)
         print ("dOdoL",posLeft0,posLeft1,posLeft1-posLeft0)
         print ("dOdoR",posRight0,posRight1,posRight1-posRight0)
 
-	spdL = delat_odo(posLeft1,posLeft0)/8.0/tloop*60.0
-	spdR = delat_odo(posRight1,posRight0)/8.0/tloop*60.0
-
-	print ("RPM Left",spdL,"RPM Right",spdR)
+        spdL = delta_odo(posLeft1,posLeft0)/8.0/tloop*60.0
+        spdR = delta_odo(posRight1,posRight0)/8.0/tloop*60.0
+        print ("RPM Left",spdL,"RPM Right",spdR)
+       
         timeAcq0 = timeAcq1
         posRight0 = posRight1
         posLeft0 = posLeft1
 
+    ard.send_arduino_cmd_motor (0,0)
+ 
