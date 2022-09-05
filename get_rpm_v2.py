5168c3dc3b7aad492dc3fb64a331ed480b7b2e83
newubu <zerrbe>
2022-09-05 16:47:27 +0200

    update encoder driver v2

--- get_rpm_v2.py
+++ get_rpm_v2.py
@@ -17,38 +17,34 @@ if __name__ == "__main__":
     timeout = 1.0
     cmdl = 50
     cmdr = 50
+    try:
+        cmdl = int(sys.argv[1])
+    except:
+        pass
+    try:
+        cmdr = int(sys.argv[2])
+    except:
+        pass        
     duration = 60.0
-    tloop = 5.0 # 0.2 Hz loop
+    tloop = 2.0 # 0.5 Hz loop
     ard = ardudrv.ArduinoIO()
     ard.send_arduino_cmd_motor (cmdl,cmdr)
-    encoddrv = encdrv.EncoderIO(old=True)      
-
-    print ("ok!")
-    #sync0, timeAcq0, sensLeft0, sensRight0, posLeft0, posRight0 =  encodrv.read_single_packet(debug=True)
-    while True:
-        sync0,data_encoders0 = encoddrv.read_packet(debug=False)
-        #print (sync0)
-        if sync0:
-            break
-        time.sleep(0.1)
-
-    print ("ok!")
-    timeAcq0 = data_encoders0[0]
-    sensLeft0 = data_encoders0[1]
-    sensRight0 = data_encoders0[2]
-    posLeft0 = data_encoders0[3]
-    posRight0 = data_encoders0[4]
-    print (sync0, timeAcq0, sensLeft0, sensRight0, posLeft0, posRight0)
+    encoddrv = encdrv.EncoderIO()      
 
     t0 = time.time()
     while (time.time()-t0) < duration:
         t0loop = time.time()
-        while (time.time()-t0loop) < tloop:
-            sync1,data_encoders1 = encoddrv.read_packet(debug=False)
-            time.sleep(0.001)
 
-        #sync1, timeAcq1, sensLeft1, sensRight1, posLeft1, posRight1 =  encodrv.read_single_packet(debug=True)
-        sync1,data_encoders1 = encoddrv.read_packet(debug=True)
+        st0,st1 = encdrv.get_last_and_older_values_v2 ()
+        data_encoders0 = np.array(st0.split(",")).astype(np.float)
+        data_encoders1 = np.array(st1.split(",")).astype(np.float)
+
+        timeAcq1 = data_encoders0[0]
+        sensLeft1 = data_encoders0[1]
+        sensRight1 = data_encoders0[2]
+        posLeft1 = data_encoders0[3]
+        posRight1 = data_encoders0[4]
+
         timeAcq1 = data_encoders1[0]
         sensLeft1 = data_encoders1[1]
         sensRight1 = data_encoders1[2]
@@ -63,10 +59,10 @@ if __name__ == "__main__":
         rpmL = delta_odo(posLeft1,posLeft0)/8.0/tloop*60.0
         rpmR = delta_odo(posRight1,posRight0)/8.0/tloop*60.0
         print ("RPM Left",rpmL,"RPM Right",rpmR)
-       
-        timeAcq0 = timeAcq1
-        posRight0 = posRight1
-        posLeft0 = posLeft1
+
+        while (time.time()-t0loop) < tloop:
+            sync1,data_encoders1 = encoddrv.read_packet(debug=False)
+            time.sleep(0.001)
 
     ard.send_arduino_cmd_motor (0,0)
  
