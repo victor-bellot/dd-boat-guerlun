079d93c8e7224dab24ffbd3d8cb202e63baebf02
newubu <zerrbe>
2022-09-07 19:56:15 +0200

    update install'

--- install.bash
+++ install.bash
@@ -6,3 +6,17 @@ git clone https://gitlab.ensta-bretagne.fr/zerrbe/drivers-ddboat-v2.git
 # update git repo
 cd drivers-ddboat-v2
 git pull
+cd /home/pi
+cd ddboat
+# may be already created
+git clone https://gitlab.ensta-bretagne.fr/zerrbe/arduino-ddboat.git arduino
+# update
+cd arduino
+git config user.email "benblop@gmail.com"
+git config user.name "Ben Blop"
+git stash
+git pull
+cd mega/cmd_motors_cpu_rc_1ch_ppm_ensv
+make && make upload
+
+
