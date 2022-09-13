dcc245941674b9571db70759319f6a8900cfb4e5
Jules Bellot <54335091+Jules156@users.noreply.github.com>
2022-09-13 09:35:28 +0200

    Update regulator

--- regulateur.py
+++ regulateur.py
@@ -1,3 +1,5 @@
+import math
+import sys
 import numpy as np
 import time
 import arduino_driver_v2
@@ -13,33 +15,41 @@ def delta_odo(odo1, odo0):
     return dodo
 
 
-def get_rpm():
+def get_rpm(enc, dt):
+    # 1 : new ; 0 : old
     st1, st0 = enc.get_last_and_older_values_v2()
     data_encoders0 = np.array(st0.split(",")).astype(np.float)
     data_encoders1 = np.array(st1.split(",")).astype(np.float)
 
-    posLeft0 = data_encoders0[4]
-    posRight0 = data_encoders0[3]
+    odo_left0 = data_encoders0[4]
+    odo_right0 = data_encoders0[3]
 
-    posLeft1 = data_encoders1[4]
-    posRight1 = data_encoders1[3]
+    odo_left1 = data_encoders1[4]
+    odo_right1 = data_encoders1[3]
 
-    rpm_left = delta_odo(posLeft1, posLeft0) / 8.0 / T_loop * 60.0
-    rpm_right = delta_odo(posRight1, posRight0) / 8.0 / T_loop * 60.0
+    rpm_left = delta_odo(odo_left1, odo_left0) / 8.0 / dt * 60.0
+    rpm_right = delta_odo(odo_right1, odo_right0) / 8.0 / dt * 60.0
 
     return rpm_left, rpm_right
 
 
-def regul():
+def regul(rpm_left_bar, rpm_right_bar, dt=1.0, cmd_left_init=50, cmd_right_init=50):
+    ard = arduino_driver_v2.ArduinoIO()
+    enc = encoders_driver_v2.EncoderIO()
+
     kp_left, ki_left = 1, 0.01
     kp_right, ki_right = 1, 0.01
 
     ei_left, ei_right = 0, 0
+    cmd_left, cmd_right = cmd_left_init, cmd_right_init
+
+    ard.send_arduino_cmd_motor(cmd_left, cmd_right)
+    enc.set_older_value_delay_v2(10)  # number of measurement during dt
 
     t0 = time.time()
     while (time.time() - t0) < duration:
         t0loop = time.time()
-        rpm_left, rpm_right = get_rpm()
+        rpm_left, rpm_right = get_rpm(enc, dt)
 
         # left motor
         e_left = rpm_left_bar - (-rpm_left)
@@ -53,32 +63,27 @@ def regul():
 
         print(rpm_left, rpm_right, u_left, u_right)
 
-        # ABSURDE : on décélère, on ne tourne pas dans l'autre sens!
-        if abs(u_left) > 255:
+        # On seuil la variation en tension
+        if abs(u_left) > 60:
             u_left = 60 * u_left/abs(u_left)
         if abs(u_right) > 60:
             u_right = 60 * u_right/abs(u_right)
 
-        ard.send_arduino_cmd_motor(u_left, u_right)
+        cmd_left += u_left
+        cmd_right += u_right
 
-        while (time.time() - t0loop) < T_loop:
+        ard.send_arduino_cmd_motor(cmd_left, cmd_right)
+
+        while (time.time() - t0loop) < dt:
             time.sleep(0.001)
+
+    ard.send_arduino_cmd_motor(0, 0)
 
 
 if __name__ == '__main__':
-    ard = arduino_driver_v2.ArduinoIO()
-    enc = encoders_driver_v2.EncoderIO()
+    try:
+        duration = int(sys.argv[1])
+    except:
+        duration = math.inf
 
-    dt = 0.1
-    cap = 0
-    cmd_left, cmd_right = 50, 50
-    rpm_left_bar, rpm_right_bar = 10000, 10000
-
-    duration = 10.0
-    T_loop = 1.0
-    ard.send_arduino_cmd_motor(cmd_left, cmd_right)
-
-    enc.set_older_value_delay_v2(10)  # 50 measurements -> 5s (10 meas/s)
-
-    regul()
-    ard.send_arduino_cmd_motor(0, 0)
+    regul(rpm_left_bar=10000, rpm_right_bar=10000)
