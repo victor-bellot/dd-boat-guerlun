9fc7047c0e977446eb965af0792e2dde4ac92d7b
Jules Bellot <54335091+Jules156@users.noreply.github.com>
2022-09-15 15:55:17 +0200

    Test kd rpm regulator

--- control.py
+++ control.py
@@ -27,16 +27,21 @@ class Control:
         self.cst = {'left': {'kp': 0.01, 'ki': 0.01},
                     'right': {'kp': 0.01, 'ki': 0.01},
                     'phi': {'kp': (3/4) / np.pi, 'ki': 1e-2 / np.pi},
-                    'line': {'kd': 150, 'kn': 1}}
+                    'line': {'kd': 150, 'kn': 1},
+                    'left_': {'kd': 2, 'kp': 0.5, 'ki': 0.1},
+                    'right_': {'kd': 2, 'kp': 0.5, 'ki': 0.1},
+                    }
 
         self.step_max = 50
         self.u_max = 100
         self.rpm_max = 4000
 
+        self.el_left, self.el_right = 0, 0
         self.ei_left, self.ei_right, self.ei_psi = 0, 0, 0
         self.cmd_left, self.cmd_right = 50, 50
 
     def reset(self, cmd_left_init=50, cmd_right_init=50):
+        self.el_left, self.el_right = 0, 0
         self.ei_left, self.ei_right, self.ei_psi = 0, 0, 0
         self.cmd_left, self.cmd_right = cmd_left_init, cmd_right_init
 
@@ -108,6 +113,35 @@ class Control:
 
         self.cmd_left = max(min(self.u_max, self.cmd_left + step_left), 0)
         self.cmd_right = max(min(self.u_max, self.cmd_right + step_right), 0)
+
+        self.ard.send_arduino_cmd_motor(self.cmd_left, self.cmd_right)
+
+        # print('MEASURED RPM:', rpm_left, rpm_right)
+        return rpm_left, rpm_right
+
+    def regulation_proportion(self, rpm_left_bar, rpm_right_bar):
+        rpm_left, rpm_right = self.get_rpm()
+
+        # left motor
+        e_left = rpm_left_bar - rpm_left
+        self.ei_left += e_left * self.dt
+        delta_e_left = (e_left - self.el_left) / self.dt
+        self.el_left = e_left
+        c_left = self.cst['left_']['kd'] * delta_e_left\
+                 + self.cst['left_']['kp'] * e_left\
+                 + self.cst['left_']['ki'] * self.ei_left
+
+        # right motor
+        e_right = rpm_right_bar - (-rpm_right)
+        self.ei_right += e_right * self.dt
+        delta_e_right = (e_right - self.el_right) / self.dt
+        self.el_right = e_right
+        c_right = self.cst['right_']['kd'] * delta_e_right\
+                  + self.cst['right_']['kp'] * e_right\
+                  + self.cst['right_']['ki'] * self.ei_right
+
+        self.cmd_left = max(min(self.u_max, c_left), 0)
+        self.cmd_right = max(min(self.u_max, c_right), 0)
 
         self.ard.send_arduino_cmd_motor(self.cmd_left, self.cmd_right)
 
@@ -194,6 +228,30 @@ class Control:
 
         self.ard.send_arduino_cmd_motor(0, 0)
 
+    def test_regulation_rpm(self, duration, rpm_spd, proportion=False):
+        self.log.write("duration: %i ; spd: %i\n" % (duration, rpm_spd))
+
+        self.reset()
+        t0 = time.time()
+        while (time.time() - t0) < duration:
+            t0loop = time.time()
+
+            if proportion:
+                rpm_left, rpm_right = self.regulation_proportion(rpm_spd, rpm_spd)
+            else:
+                rpm_left, rpm_right = self.regulation_rpm(rpm_spd, rpm_spd)
+
+            print(rpm_left, rpm_right)
+
+            data = [(t0loop - t0) * 1000, rpm_left, rpm_right]
+            information = data_to_str(data)
+            self.log.write(information)
+
+            while time.time() - t0loop < self.dt:
+                time.sleep(0.01)
+
+        self.ard.send_arduino_cmd_motor(0, 0)
+
 
 if __name__ == '__main__':
     print("--- Control program ---\n")
@@ -201,7 +259,7 @@ if __name__ == '__main__':
     mn = input("Mission name: ")
     ctr = Control(mn)
 
-    mt = input("Mission type (psi, square, line): ")
+    mt = input("Mission type (psi, square, line, test): ")
 
     if mt == 'line':
         a = input("Starting point: ")
@@ -227,6 +285,18 @@ if __name__ == '__main__':
         ctr.follow_psi(d, speed_rpm=s, psi_bar=cap_to_psi('W'))
         ctr.follow_psi(d, speed_rpm=s, psi_bar=cap_to_psi('S'))
         ctr.follow_psi(d, speed_rpm=s, psi_bar=cap_to_psi('N'))
+
+    elif mt == 'test':
+        d_input = input("Test duration: ")
+        d = math.inf if d_input == '' else int(d_input)
+
+        s_input = input("Boat RPM speed: ")
+        s = 3000 if s_input == '' else int(s_input)
+
+        p_input = input("Proportion (0/1): ")
+        p = (p_input == 1)
+
+        ctr.test_regulation_rpm(d, rpm_spd=s, proportion=p)
 
     else:
         d_input = input("Mission duration: ")
