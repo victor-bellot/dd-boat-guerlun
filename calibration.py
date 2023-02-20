19b828bc56f1509532e6f9d15d282f6671e637dc
Jules Bellot <54335091+Jules156@users.noreply.github.com>
2023-02-20 14:52:28 +0100

    Add arduino_driver_v2.py

--- calibration.py
+++ calibration.py
@@ -36,4 +36,4 @@ if __name__ == "__main__":
                     time.sleep(0.01)
                 res[i] = np.median(measurements, axis=1, keepdims=True)
                 print("%s = (%f, %f, %f)" % (label, *list(res[i].flatten())))
-            np.save(f, res)
+            np.save(file_name, res)
